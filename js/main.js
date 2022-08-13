d910ae7bb7a0587d6c3c60661f8241c7296cd747
Tilak Dave <davetilak003@gmail.com>
2022-08-14 01:07:12 +0530

    hmm

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ fetch("http://127.0.0.1:5500/data.json")
         },
         options: {
             responsive: true,
-            maintainAspectRatio: false,
+            // maintainAspectRatio: false,
         }
     });
 
